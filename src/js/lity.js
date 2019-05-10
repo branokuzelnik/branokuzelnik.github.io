593738294d1567578ebea9268c34d38ee02decd1
azurry <theazurry@gmail.com>
2019-05-10 22:18:07 +0200

    Update lity.js

--- src/js/lity.js
+++ src/js/lity.js
@@ -1,4 +1,4 @@
 /* azurry's bad jquery attempt, pls no hate, i can't be bothered to learn all this stuff rn so it's a mess */
 if ($(window).width() > 840) {
-	$.getScript( "http://azurry.com/ASSETS/js/lity-main.js" );
+	$.getScript( "http://branokuzelnik.eu/src/js/lity-main.js" );
 }
